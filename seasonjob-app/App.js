977214335b5e20fd1f80f88600c32ec41ae5413b
ach-ber <77842805+ach-ber@users.noreply.github.com>
2023-09-29 16:53:31 +0200

    change structure

--- seasonjob-app/App.js
+++ seasonjob-app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignInScreen from './screens/SignInScreen';
+import SignInScreen from './src/screens/connection/SignInScreen';
 
 const Stack = createNativeStackNavigator();
 
