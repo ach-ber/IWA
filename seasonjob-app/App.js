36bd252bef4826992d7b8952eabec03b31f16b56
ach-ber <77842805+ach-ber@users.noreply.github.com>
2023-10-04 10:40:14 +0200

    clean code

--- seasonjob-app/App.js
+++ seasonjob-app/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from './src/screens/connection/SignInScreen';
+import SignUpScreen from './src/screens/connection/SignUpScreen';
 import AccountScreen from "./src/screens/account/AccountScreen";
-import SignUpScreen from "./src/screens/connection/SignUpScreen";
 import FormuleScreen from "./src/screens/account/FormuleScreen";
 
 const Stack = createNativeStackNavigator();
@@ -11,11 +11,11 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-          <Stack.Screen name="SignIn" component={SignInScreen} />
-          <Stack.Screen name="Account" component={AccountScreen} />
-          <Stack.Screen name="SignUp" component={SignUpScreen} />
-          <Stack.Screen name="Formule" component={FormuleScreen} />
+      <Stack.Navigator initialRouteName="Connexion">
+        <Stack.Screen name="Connexion" component={SignInScreen} options={{ headerShown: false, gestureEnabled: false }} />
+        <Stack.Screen name="Inscription" component={SignUpScreen} options={{ headerShown: false, gestureEnabled: false }} />
+        <Stack.Screen name="Compte" component={AccountScreen} options={{ headerShown: false, gestureEnabled: false }} />
+        <Stack.Screen name="Formule" component={FormuleScreen} options={{ headerShown: false, gestureEnabled: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ seasonjob-app/src/localization/localization.js
@@ -0,0 +1,42 @@
+// more info about localization (plural, interpolation, etc.):
+// https://phrase.com/blog/posts/react-native-i18n-with-expo-and-i18next-part-1/
+
+const translations = {
+    en: {
+        signin: "Sign In",
+        signin_screen: "Sign In Screen",
+        signup: "Sign Up",
+        signup_screen: "Sign Up Screen",
+        enter_complete_name: "Please enter your full name",
+        enter_email: "Please enter your email address",
+        enter_valid_email: "Please fix email format",
+        enter_valid_password: "Password must be at least 8 characters long",
+        full_name: "Full Name",
+        email: "Email",
+        password: "Password",
+        min_8_characters: "8 characters minimum",
+        already_have_account: "Already have an account ?",
+        forgot_password: "Forgot password ?",
+        dont_have_account: "Don't have an account ?",
+    },
+
+    fr: {
+        signin: "Se connecter",
+        signin_screen: "Écran de connexion",
+        signup: "S'inscrire",
+        signup_screen: "Écran d'inscription",
+        enter_complete_name: "Veuillez entrer votre nom complet",
+        enter_email: "Veuillez entrer votre adresse e-mail",
+        enter_valid_email: "Veuillez corriger le format d'email",
+        enter_valid_password: "Le mot de passe doit contenir au moins 8 caractères",
+        full_name: "Nom complet",
+        email: "Adresse mail",
+        password: "Mot de passe",
+        min_8_characters: "8 caractères minimum",
+        already_have_account: "Vous avez déjà un compte ?",
+        forgot_password: "Mot de passe oublié ?",
+        dont_have_account: "Vous n'avez pas de compte ?",
+    },
+}
+
+export default translations;
