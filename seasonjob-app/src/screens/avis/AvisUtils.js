fe9bce0f5ad42eab32b0d3d544b3c54877c63841
Océan Sicard--Baux <ocean.sicard-baux@etu.umontpellier.fr>
2023-11-28 12:43:05 +0100

    fix reviews

--- seasonjob-app/src/screens/avis/AvisUtils.js
+++ seasonjob-app/src/screens/avis/AvisUtils.js
@@ -1,20 +1,20 @@
 const transformEpochToDate = function (timestamp) {
     // date format: dd-mm-yyyy
-    const date = new Date(timestamp * 1000)
+    const date = timestamp.toString().length === 13 ? new Date(timestamp) : new Date(timestamp * 1000)
     return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
 }
 
 const transformAvis = function (listAvis) {
     const transformedAvis = listAvis.map(avis => {
         return {
-            id: avis.id,
-            nom: "nom",
-            prenom: "prenom",
-            job: "job",
-            note: avis.rating,
-            date: transformEpochToDate(avis.createdAt),
-            titre: avis.title,
-            avis: avis.comment,
+            id: avis.review.id,
+            nom: avis.candidate ? avis.candidate.firstname : "nom",
+            prenom: avis.candidate ? avis.candidate.lastname : "prenom",
+            job: "job n°" + avis.review.jobId,
+            note: avis.review.rating,
+            date: transformEpochToDate(avis.review.createdAt),
+            titre: avis.review.title,
+            avis: avis.review.comment,
         }
     })
     return transformedAvis
